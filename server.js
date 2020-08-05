abab1635cfa73c6bec3456888f19bdafe40796f7
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-08-05 18:21:08 -0400

    setup file strucure for nav bar & image upload

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
+//reference to dependecies
+const { cloudinary } = require('./utils/cloudinary');
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+//get a list of public id's from the file folder in cloudinary
+app.get('/api/images', async (req, res) => {
+  const { resources } = await cloudinary.search
+      .expression('folder:imageselector')
+      .sort_by('public_id', 'desc')
+      .max_results(5)
+      .execute();
+//sends back an array of ID's to be tested on the front end (localhot:3001/api/images)
+  const publicIds = resources.map((file) => file.public_id);
+  res.send(publicIds);
+});
+app.post('/api/upload', async (req, res) => {
+  try {
+      const fileStr = req.body.data;
+      const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+          //references the folder where files will be stored within cloudinary site
+          upload_preset: 'imageselector',
+      });
+      console.log(uploadResponse);
+      res.json({ msg: 'Awesome--> Image loaded' });
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ err: 'Uh Oh Danger Sir Robinson Danger---> failed' });
+  }
+});
+
 const express = require("express");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+//set image upload size
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +55,9 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 
 // Start the API server
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+
