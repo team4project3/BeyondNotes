0c43da4c36a17860f167f4dc82ebfa4c008d8d06
Ashley Feese <58886725+AFeese@users.noreply.github.com>
2020-08-13 19:21:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 // Start the API server
 const PORT = process.env.PORT || 3001;
