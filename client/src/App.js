bddf6944605139c9b731e0e89412ea7ad642287e
AFeese <58886725+AFeese@users.noreply.github.com>
2020-08-18 20:31:46 -0400

    fixed page rendering....?

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -10,13 +10,13 @@ import Upload from './pages/Upload.js';
 import Gallery from './pages/Gallery.js';
 import LoginForm from './components/LoginForm/LoginForm';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm';
-import AlertComponent from './components/AlertComponent/AlertComponent'; 
+import AlertComponent from './components/AlertComponent/AlertComponent';
 import Contacts from "./components/Contacts";
 import "./App.css";
 import AddContact from "./components/AddContact";
 import EditContact from "./components/EditContact";
 import ContactsContextProvider from "./components/ContactsContextProvider";
-import Quotes from "../src/components/Quotes/Quote.js";
+// import Quotes from "../src/components/Quotes/Quote.js";
 
 
 
@@ -26,43 +26,48 @@ function App() {
   const [errorMessage, updateErrorMessage] = useState(null);
   return (
     <Router>
-      <div> 
-      {/* <ContactsContextProvider> */}
+      <div>
+        {/* <ContactsContextProvider> */}
         <StoreProvider>
           {/* <ContactsContextProvider> */}
           <Switch>
             {/* <Route exact path="/" component={Home} /> */}
-            <Route exact path="/home" component={Home} />
-            <Route path="/" exact={true}>
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
-            </Route>
+
             <Route path="/register">
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
+
             <Route path="/login">
-              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
-            <Route exact path="/calendar" component={Calendar}/>
+
+            <Route exact path="/home" component={Home} />
+
+            <Route path="/" exact={true}>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle} />
+            </Route>
 
 
-            <Route exact path="/contacts" component={Contacts} /> 
+            <Route exact path="/calendar" component={Calendar} />
+            <Route exact path="/notelist" component={NoteList} />
+            <Route exact path="/posts/:id" component={Detail} />
+
+            <Route exact path="/contacts" component={Contacts} />
             <Route path="/add" component={AddContact} />
-            <Route path="/:id" component={EditContact} /> 
-    
+            <Route path="/:id" component={EditContact} />
+
             {/* <Route exact path="/quotes" component={Quotes}></Route> */}
             {/* <Route exact path="/favorites" component={FavoritesList} /> */}
 
-            <Route exact path="/notelist" component={NoteList} />
 
-            <Route exact path="/posts/:id" component={Detail} />
-    
-            <Route exact path="/gallery" component={Gallery} />  
-    
+
+            <Route exact path="/gallery" component={Gallery} />
             <Route component={Upload} path="/upload" />
-          <Route component={NoMatch} />
+            <Route component={NoMatch} />
+
           </Switch>
 
-          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage} />
           {/* </ContactsContextProvider>  */}
         </StoreProvider>
         {/* </ContactsContextProvider> */}
