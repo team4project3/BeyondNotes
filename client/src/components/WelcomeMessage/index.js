9ee84dd428f0cc5cd4ba30150431e00c3953d35f
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-08-22 12:27:12 -0400

    fix issue with getting the email to render for the welcome message

--- client/src/components/WelcomeMessage/index.js
+++ client/src/components/WelcomeMessage/index.js
@@ -1,24 +1,69 @@
 import React, { useEffect, useState } from 'react';
+import API from '../../utils/API';
 
 const User = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState({
+    username: ""
+  });
+
+  // async function getUsers() {
+  //   const response = await fetch('/api/user_data');
+  //   const users = await response.json();
+  //   setUsers(users);
+  // }
+
+
+  const fetchData = async () => {
+    try {
+        const response = await fetch('/api/user_data');
+        const users = await response.json();
+      setUsers({username:users.email})
+        return users;
+    } catch (error) {
+        throw error;
+    }
+};
+console.log(users)
 
   useEffect(() => {
-    getUsers();
+      fetchData()
+          // .then(users)
+          // .catch(error => {
+          //     console.warn(JSON.stringify(error, null, 2));
+          // });
   }, []);
 
-  async function getUsers() {
-    const response = await fetch('/api/user_data');
-    const users = await response.json();
-    setUsers(users);
-  }
+
+
+
+
+  // useEffect(() => {
+  //   // const response = await fetch('/api/user_data');
+  //   const response =  fetch('/api/user_data');
+  //   // const users = await response.json();
+  //   const users =  response.json();
+  //   API.getUsers.then((response) => {
+  //     setUsers(users);
+  //     // console.log(username);
+  //     console.log(users);
+  //   });
+  // }, []);
+
+  // async function getUsers() {
+  //   const response = await fetch('/api/user_data');
+  //   const users = await response.json();
+  //   setUsers(users);
+  // }
 
   return (
-    <ul>
-      {users.map(user => (
-        <li>{user.name}</li>
-      ))}
-    </ul>
+    <div>
+      <h1>{users.username}</h1>
+    </div>
+    // <ul>
+    //   {users.map(user => (
+    //     <li>{user.name}</li>
+    //   ))}
+    // </ul>
   );
 };
 
